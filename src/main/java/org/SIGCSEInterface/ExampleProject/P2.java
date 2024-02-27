7829f739ebc05aab12f22af065d6972446b2525a
ProfLupoli <slupoli@tamu.edu>
2024-02-27 10:04:17 -0500

    add HelloWorld.java

---
+++ src/main/java/org/SIGCSEInterface/ExampleProject/P2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.SIGCSEInterface.ExampleProject;
+
+/**
+ *
+ * @author Luper
+ */
+public class P2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Luper's World!");
+    }
+}
